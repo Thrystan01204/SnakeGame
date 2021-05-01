bf8987684b3bacdbf3ff1f09b0274cc1abb278e3
Stryxman <stryxman18@gmail.com>
2021-05-01 11:59:53 +0400

    Add: menu incoming

---
+++ app/src/main/java/com/example/snakegame/MainGame.java
@@ -0,0 +1,137 @@
+package com.example.snakegame;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+
+/**
+ * Created by <VOTRE-NOM> on <DATE-DU-JOUR>.
+ * Project Name
+ */
+public class MainGame extends AppCompatActivity {
+    public static ImageView img_swipe;
+    public static Dialog dialogScore;
+    private GameView gv;
+    public static TextView txt_score, txt_best_score, txt_dialog_score, txt_dialog_best_score;
+    public static ArrayList<Integer> scores = new ArrayList<Integer>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        Constants.SCREEN_WIDTH = dm.widthPixels;
+        Constants.SCREEN_HEIGHT = dm.heightPixels;
+        setContentView(R.layout.activity_game);
+        img_swipe = findViewById(R.id.img_swipe);
+        gv = findViewById(R.id.gv);
+        txt_score = findViewById(R.id.txt_score);
+        txt_best_score = findViewById(R.id.txt_best_score);
+        dialogScore();
+
+
+    }
+
+
+    private void openScore(){
+        Intent intent = new Intent(this, ListeBestScore.class);
+        startActivity(intent);
+    }
+
+    private void writeToFile(ArrayList<Integer> arrayList, Context context){
+        try {
+            OutputStreamWriter osw = new OutputStreamWriter(context.openFileOutput("score.txt", Context.MODE_PRIVATE));
+            for(int i = 0; i < arrayList.size()- 1; i++){
+                osw.write(arrayList.get(i));
+            }
+
+            osw.close();
+        } catch (IOException error){
+            Log.e("Exception", "File Write failed" + error.toString());
+        }
+    }
+
+    private String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append("\n").append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    private void dialogScore() {
+        int bestScore = 0;
+        SharedPreferences sp = this.getSharedPreferences("gamesetting", Context.MODE_PRIVATE);
+        if(sp!=null){
+            bestScore = sp.getInt("bestscore",0);
+        }
+        MainGame.txt_best_score.setText(bestScore+"");
+        dialogScore = new Dialog(this);
+        dialogScore.setContentView(R.layout.dialog_start);
+        txt_dialog_score = dialogScore.findViewById(R.id.txt_dialog_score);
+        txt_dialog_best_score = dialogScore.findViewById(R.id.txt_dialog_best_score);
+        txt_dialog_best_score.setText(bestScore + "");
+        dialogScore.setCanceledOnTouchOutside(false);
+        RelativeLayout rl_start = dialogScore.findViewById(R.id.rl_start);
+        rl_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                img_swipe.setVisibility(View.VISIBLE);
+                gv.reset();
+                dialogScore.dismiss();
+            }
+        });
+        RelativeLayout rl_scores = dialogScore.findViewById(R.id.rl_score);
+        rl_scores.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openScore();
+            }
+        });
+        dialogScore.show();
+    }
+}
+
